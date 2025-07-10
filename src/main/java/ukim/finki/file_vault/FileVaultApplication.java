8a7445bd691899f0ade346b22794eedbad7bfe64
martin <martingrombanovski@yahoo.com>
2025-07-10 19:17:13 +0200

    Initial setup

---
+++ src/main/java/ukim/finki/file_vault/FileVaultApplication.java
@@ -0,0 +1,13 @@
+package ukim.finki.file_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ukim/finki/file_vault/FileVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package ukim.finki.file_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
