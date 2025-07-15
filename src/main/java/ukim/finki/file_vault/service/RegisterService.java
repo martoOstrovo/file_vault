a326687099d6b83d7c21a5303da739f6e8eb4621
martin <martingrombanovski@yahoo.com>
2025-07-16 01:10:30 +0200

    made registerService into an interface and separate implementation class

--- src/main/java/ukim/finki/file_vault/service/RegisterService.java
+++ src/main/java/ukim/finki/file_vault/service/RegisterService.java
@@ -1,34 +1,7 @@
 package ukim.finki.file_vault.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import ukim.finki.file_vault.model.Role;
-import ukim.finki.file_vault.model.User;
 import ukim.finki.file_vault.model.UserDTO;
-import ukim.finki.file_vault.repository.RoleRepository;
-import ukim.finki.file_vault.repository.UserRepository;
 
-@Service
-public class RegisterService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleRepository roleRepository;
-
-    public RegisterService(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public void registerUser(UserDTO userDTO) {
-        User user = new User();
-        user.setName(userDTO.getName());
-        user.setSurname(userDTO.getSurname());
-        user.setEmail(userDTO.getEmail());
-        user.setUsername(userDTO.getUsername());
-        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-        Role role = roleRepository.findByRoleName("ROLE_USER").orElseThrow(() -> new RuntimeException("Default Role Not Found"));
-        user.getRoles().add(role);
-        userRepository.save(user);
-    }
+public interface RegisterService {
+    void registerUser(UserDTO userDTO);
 }
---
+++ src/main/java/ukim/finki/file_vault/service/implementation/RegisterServiceImp.java
@@ -0,0 +1,35 @@
+package ukim.finki.file_vault.service.implementation;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ukim.finki.file_vault.model.Role;
+import ukim.finki.file_vault.model.User;
+import ukim.finki.file_vault.model.UserDTO;
+import ukim.finki.file_vault.repository.RoleRepository;
+import ukim.finki.file_vault.repository.UserRepository;
+import ukim.finki.file_vault.service.RegisterService;
+
+@Service
+public class RegisterServiceImp implements RegisterService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    public RegisterServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setSurname(userDTO.getSurname());
+        user.setEmail(userDTO.getEmail());
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        Role role = roleRepository.findByRoleName("ROLE_USER").orElseThrow(() -> new RuntimeException("Default Role Not Found"));
+        user.getRoles().add(role);
+        userRepository.save(user);
+    }
+}
